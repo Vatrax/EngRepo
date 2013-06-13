ca0cc4f92bf3e6353f8ccbcf2f1c84becd0ba18b
Wojciech Krzaczek <krzaczekw@gmail.com>
2013-06-13 09:28:20 +0200

    WPF final

---
+++ __Programy/C#/.Net i Java/odtwarzacz WPF/odtwarzacz WPF/Playlistelement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odtwarzacz_WPF
+{
+    public class Playlistelement
+    {
+        string _sourcedata;
+        string _namedata;
+        public Playlistelement(string source,string name) 
+        {
+            _sourcedata = source;
+            _namedata = name;
+        }
+        public string sourcedata
+        {
+            get { return _sourcedata; }
+            set { _sourcedata = value; }
+        }
+        public string namedata
+        {
+            get { return _namedata; }
+            set { _namedata = value; }
+        }
+    }
+}
