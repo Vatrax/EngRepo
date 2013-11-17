3df9f8cc3c444bffeb19be0920a2e04369dbddc6
Vatrax <krzaczekw@gmail.com>
2013-11-17 23:19:31 +0100

    removing useless dependencies

--- __Programy/Java/ws/src/main/java/pl/sample/rest/RestResolver.java
+++ __Programy/Java/ws/src/main/java/pl/sample/rest/RestResolver.java
@@ -3,7 +3,6 @@ package pl.sample.rest;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.ejb.Stateless;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -11,7 +10,6 @@ import javax.ws.rs.core.Application;
  * @author: Wojciech Krzaczek
  */
 
-@Stateless
 @ApplicationPath("/rest")
 public class RestResolver extends Application {
 	@Override
