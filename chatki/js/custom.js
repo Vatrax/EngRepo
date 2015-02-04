8b9c0631195a16feaa6330157253f09f83b7fa1d
krzaczek <krzaczekw@gmail.com>
2015-02-04 14:35:05 +0100

    tidy up

---
+++ chatki/js/custom.js
@@ -0,0 +1,7 @@
+var totalCount = 8;
+function selectImage() 
+{
+var num = Math.ceil( Math.random() * totalCount );
+document.body.background = 'images/atractions/'+num+'.jpg';
+document.body.style.backgroundRepeat = "repeat";// Background repeat
+}
